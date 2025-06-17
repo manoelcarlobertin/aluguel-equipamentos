36283f2443799cec5c5d573d8474af1973cf21e2
BertinManoelBr <manoelcarlobertin@outlook.com>
2025-06-17 08:04:06 -0300

    entendendo o metodo de classe que lista todos equipos disponiveis, não agendados inclusive, e filtra com o where os sem agendamento OU equipos que NAO ocupam o periodo definido.

--- app/models/equipament.rb
+++ app/models/equipament.rb
@@ -1,4 +1,11 @@
 class Equipament < ApplicationRecord
   has_many :schedules
   has_many :rents
+
+  def self.availables(period_start, period_end)
+    left_outer_joins(:schedules) # traz todos os equipamentos.
+      .where("schedules.id IS NULL OR NOT (schedules.period_start <= ? AND schedules.period_end >= ?)", period_end, period_start)
+      # a condição where filtra os sem agendamento OU equipos que NÃO sobrepôem perído informado.
+      .distinct # evita equipos repetidos na lista.
+  end
 end
