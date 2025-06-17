96f706560a23700186bec2d62b4247c5ba39b5a8
BertinManoelBr <manoelcarlobertin@outlook.com>
2025-06-17 09:12:51 -0300

    criei uma validacao de datas para os filtros, mas futuramente irei coloca-las num helper

--- app/controllers/equipaments_controller.rb
+++ app/controllers/equipaments_controller.rb
@@ -1,12 +1,30 @@
 class EquipamentsController < ApplicationController
   before_action :set_equipament, only: %i[ show edit update destroy ]
 
+  # validações
   def available
-    period_start = params[:period_start].present? ? Date.parse(params[:period_start]) : Date.today - 7.days
-    period_end = params[:period_end].present? ? Date.parse(params[:period_end]) : Date.today + 2.days
+    begin
+      period_start = params[:filter] && params[:filter][:period_start].present? ? Date.parse(params[:filter][:period_start]) : Date.today
+      period_end = params[:filter] && params[:filter][:period_end].present? ? Date.parse(params[:filter][:period_end]) : Date.today + 7.days
 
-    @equipaments = Equipament.availables(period_start, period_end)
+      if period_end < period_start
+        flash.now[:alert] = "Data fim não pode ser anterior à data início."
+        @equipaments = Equipament.none
+      else
+        @equipaments = Equipament.availables(period_start, period_end)
+      end
+    rescue ArgumentError
+      flash.now[:alert] = "Datas inválidas. Por favor, insira datas no formato correto."
+      @equipaments = Equipament.none
+    end
   end
+
+  # def available
+  #   period_start = params[:period_start].present? ? Date.parse(params[:period_start]) : Date.today - 7.days
+  #   period_end = params[:period_end].present? ? Date.parse(params[:period_end]) : Date.today + 2.days
+
+  #   @equipaments = Equipament.availables(period_start, period_end)
+  # end
 
   def index
     @equipaments = Equipament.all
